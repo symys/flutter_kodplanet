6ee62613258d1d7b3042e4d7844b237446526532
symdmr <seymademir35@gmail.com>
2022-03-11 10:56:07 +0300

    weather_app_demo

--- weather_app_u/lib/home_page.dart
+++ weather_app_u/lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app_u/search_page.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,10 +14,31 @@ class _HomePageState extends State<HomePage> {
   String sehir = 'Ankara';
   int sicaklik = 20;
   var locationData;
+  var woeid;
+  var temp;
 
   Future<void> getLocation() async {
     locationData = await http.get(Uri.parse(
         'https://www.metaweather.com/api/location/search/?query=Ankara'));
+    var locationDataParsed = jsonDecode(locationData.body);
+    woeid = locationDataParsed[0]['woeid'];
+  }
+
+  Future<void> getTemperature() async {
+    var temperatureData = await http
+        .get(Uri.parse('https://www.metaweather.com/api/location/$woeid/'));
+    var temperatureDataParsed = jsonDecode(temperatureData.body);
+    temp = temperatureDataParsed['consolidated_weather'][0]['the_temp'];
+    print(temp);
+  }
+
+  @override
+  void initState() {
+    getLocation();
+    print('woeid: $woeid');
+    getTemperature();
+    // TODO: implement initState
+    super.initState();
   }
 
   @override
@@ -32,17 +54,19 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              FlatButton(
+              /*FlatButton(
                 onPressed: () async {
                   await getLocation();
                   print('http getten sonra calısan kod : $locationData');
+                  //print(locationData.body);
+                  //print(locationData.body.runtimeType); //burda tipini görüyoruz
                   //Future.delayed(Duration(seconds: 5), () {
                     //print('http getten sonra calısan kod : $locationData');
                   //});
                 },
                 child: Text('getLocation'),
                 color: Colors.pink,
-              ),
+              ),*/
               Text('$sicaklik °C',
                   style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
               Row(
